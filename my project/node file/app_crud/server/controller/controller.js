b194874b65b5ece7d83ea7253705aafba55756ba
khankhal007 <khankhaldhaval007@gmail.com>
2022-05-30 12:55:42 +0530

    30-05-22

--- my project/node file/app_crud/server/controller/controller.js
+++ my project/node file/app_crud/server/controller/controller.js
@@ -1,49 +1,56 @@
 const userModel = require("../model/userModel");
 
-// exports.signup = (async (req, res) => {
-//     console.log("hello..");
-//     try {
+exports.signup = (async (req, res) => {
+    console.log("hello..");
+    try {
 
-//         const password = req.body.password
-//         const Cpassword = req.body.Cpassword
+        const password = req.body.password
+        const Cpassword = req.body.Cpassword
 
-//         if (password === Cpassword) {
-//             const data = await userModel.create(req.body).then(res.json({"msg":"insert Data.."})).catch(e => console.log(e));
-//             console.log(data);
-//         } else {
-//            res.send({"msg":"password not sema"});
-//         }
-//     } catch {
-//         res.send({ "msg": "ERROR.." })
-//     }
-// });
-// exports.Alldatafind = (async(req,res)=>{
-//     try{
-//         const data = await userModel.find(req.body).then(res.json({"msg":req.body})).catch(e=>console.log(e));
-//         console.log(data)
-//     }catch{
-//         res.json({"msg":"no data.."})
-//     }
-// })
-exports.login = (async(req,res)=>{
-    console.log("hello login..");
+        if (password === Cpassword) {
+            const registerUser= new userModel({
+                name:req.body.name,
+                email:req.body.email,
+                mobailNo:req.body.mobailNo,
+                password:password,
+                Cpassword:Cpassword
+            })
+            const db = await registerUser.seva().then(data=>console.log("insert Data..",data));
+            console.log(db);
+        } else {
+           res.send({"msg":"password not sema"});
+        }
+    } catch(error) {
+        res.status(400).send( "ERROR.." );
+    }
+});
+exports.Alldatafind = (async(req,res)=>{
     try{
-        const data = await userModel.findOne(req.body).then(console.log("find data")).catch(e=>console.log(e));
+        const data = await userModel.find(req.body).then(res.json({"msg":req.body})).catch(e=>console.log(e));
         console.log(data)
-        if(data===""){
-            res.send("plz sigup..");
+    }catch{
+        res.json({"msg":"no data.."})
+    }
+})
+exports.login = (async(req,res)=>{
+    // console.log("hello login..");
+    try{
+        const data = await userModel.find(req.body).then(console.log("find data")).catch(e=>console.log(e));
+        console.log(data)
+        if(data == ""){
+            res.json({"msg":"plz sigup.."});
             console.log("jhjkkjk")
         }else{
             const signup_email=data[0].email;
             const login_email = req.body.email;
-            console.log(signup_email);
+            // console.log(signup_email);
             
             
-            // if(login_email == signup_email){
-            //     await userModel.find(req.body).then(console.log("login successfull")).catch(e=>console.log(e));
-            // }else{
-            //     res.json({"msg":"login fail...."})
-            // }
+            if(login_email === signup_email){
+                await userModel.create(req.body).then(res.send("login successfull")).catch(e=>console.log(e));
+            }else{
+                res.json({"msg":"login fail...."})
+            }
         }
     }
     catch(error){
--- my project/node file/app_crud/server/model/userModel.js
+++ my project/node file/app_crud/server/model/userModel.js
@@ -1,10 +1,27 @@
 const mongoose=require("mongoose");
 
 const userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    Cpassword:{type:String,required:true}
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    mobailNo:{
+        type:String,
+        required:true,
+        min:10
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    Cpassword:{
+        type:String,
+        required:true
+    }
 })
 
 var userModel = mongoose.model("userData",userSchema);
