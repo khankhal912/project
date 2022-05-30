ee46a42bcb1d5eb53a683df87ee9be78f1811624
khankhan912 <khankhandhaval007@gmail.com>
2022-05-30 14:30:04 +0530

    30-05

--- my project/node file/app_crud/server/controller/controller.js
+++ my project/node file/app_crud/server/controller/controller.js
@@ -1,59 +1,65 @@
 const userModel = require("../model/userModel");
 
 exports.signup = (async (req, res) => {
-    console.log("hello..");
+    // console.log("hello..");
     try {
 
         const password = req.body.password
         const Cpassword = req.body.Cpassword
+        console.log("helloooo")
 
         if (password === Cpassword) {
-            const registerUser= new userModel({
-                name:req.body.name,
-                email:req.body.email,
-                mobailNo:req.body.mobailNo,
-                password:password,
-                Cpassword:Cpassword
+            const registerUser = new userModel({
+                name: req.body.name,
+                email: req.body.email,
+                mobailNo: req.body.mobailNo,
+                password: password,
+                Cpassword: Cpassword
             })
-            const db = await registerUser.seva().then(data=>console.log("insert Data..",data));
+            const db = await registerUser.save(req.body).then(res.json({ "msg": "insert Data.." })).catch(e => console.log(e));
             console.log(db);
         } else {
-           res.send({"msg":"password not sema"});
+            res.send("password not sema");
         }
-    } catch(error) {
-        res.status(400).send( "ERROR.." );
+    } catch (error) {
+        res.send("ERROR..");
     }
 });
-exports.Alldatafind = (async(req,res)=>{
-    try{
-        const data = await userModel.find(req.body).then(res.json({"msg":req.body})).catch(e=>console.log(e));
+exports.Alldatafind = (async (req, res) => {
+    try {
+        const data = await userModel.find(req.body).then(res.json({ "msg": req.body })).catch(e => console.log(e));
         console.log(data)
-    }catch{
-        res.json({"msg":"no data.."})
+    } catch {
+        res.json({ "msg": "no data.." })
     }
 })
-exports.login = (async(req,res)=>{
+exports.login = (async (req, res) => {
     // console.log("hello login..");
-    try{
-        const data = await userModel.find(req.body).then(console.log("find data")).catch(e=>console.log(e));
-        console.log(data)
-        if(data == ""){
-            res.json({"msg":"plz sigup.."});
-            console.log("jhjkkjk")
-        }else{
-            const signup_email=data[0].email;
-            const login_email = req.body.email;
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            res.json({ "msg": "plz sigup.." });
+            // console.log("jhjkkjk")
+        } else {
+            const data = await userModel.findOne({ email })
+            console.log(data)
+
+            // const signup_password = data[0].password;
+            // const login_password = req.body.password;
             // console.log(signup_email);
-            
-            
-            if(login_email === signup_email){
-                await userModel.create(req.body).then(res.send("login successfull")).catch(e=>console.log(e));
-            }else{
-                res.json({"msg":"login fail...."})
+
+
+            if (password === data.password) {
+
+                console.log(data.password)
+                await userModel.find().then(res.send("login successfull")).catch(e => console.log(e));
+                // console.log(db);
+            } else {
+                res.json({ "msg": "login fail...." })
             }
         }
     }
-    catch(error){
+    catch (error) {
         res.send("ERROR..")
     }
 })
